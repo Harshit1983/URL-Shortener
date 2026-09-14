5396f4be5f5c6776b9729298778f4c7faa75e783
{Harshit} <{harshitpandiyar1983@gmail.com}>
2026-09-14 12:52:28 +0530

    Fix CORS origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+
 const cors = require('cors');
+
 const urlRoutes = require('./routes/urls');
+
 const authRoutes = require('./routes/auth');
+
 const indexRoutes = require('./routes/index');
+
 const linksRouter = require('./routes/links');
+
 const errorHandler = require('./middleware/errorMiddleware');
+
 const cookieParser = require('cookie-parser');
 
 const app = express();
@@ -16,11 +23,15 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.use('/api', urlRoutes);
+
 app.use('/api/auth', authRoutes);
+
 app.use('/', indexRoutes);
+
 app.use('/api/links', linksRouter);
 
 app.use(errorHandler);
