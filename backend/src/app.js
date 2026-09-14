a0c8ce6b300614a10e0a028a63c1e575f7b16abf
{Harshit} <{harshitpandiyar1983@gmail.com}>
2026-09-14 12:47:11 +0530

    Add CORS support

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,29 +1,28 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
-
+const cors = require('cors');
 const urlRoutes = require('./routes/urls');
 const authRoutes = require('./routes/auth');
 const indexRoutes = require('./routes/index');
 const linksRouter = require('./routes/links');
-
 const errorHandler = require('./middleware/errorMiddleware');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
-// Middlewares
+app.use(cors({
+    origin: [
+        'http://localhost:5173'
+    ]
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
 app.use('/api', urlRoutes);
-
 app.use('/api/auth', authRoutes);
-
+app.use('/', indexRoutes);
 app.use('/api/links', linksRouter);
 
-app.use('/', indexRoutes);
-
-// Error middleware must be last
 app.use(errorHandler);
 
 module.exports = app;
